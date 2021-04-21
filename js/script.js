e114b0b1c5201db6735fcb7a30b32e48d4594bea
Ivan <fenomen_ny@mail.ru>
2021-04-21 14:43:06 +0300

    practice

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 'use strict';
 
-let a = true,
-	b = false;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-	console.log(a || !b);
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const a  = prompt('Один из просмотренных фильмов?', ''),
+	b = prompt('На сколько оцените его?', ''),
+	c = prompt('Один из просмотренных фильмов?', ''),
+	d = prompt('На сколько оцените его?', '');
+
+	personalMovieDB.movies[a] = b;
+	personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
