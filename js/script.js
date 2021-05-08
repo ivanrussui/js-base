9c94aa5942fa48142ca00006978a2f5bf6acb4ca
Ivan <fenomen_ny@mail.ru>
2021-05-08 23:18:02 +0300

    practice 2

--- js/script.js
+++ js/script.js
@@ -3,20 +3,69 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
-const a  = prompt('Один из просмотренных фильмов?', ''),
-	b = prompt('На сколько оцените его?', ''),
-	c = prompt('Один из просмотренных фильмов?', ''),
-	d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
 
-	personalMovieDB.movies[a] = b;
-	personalMovieDB.movies[c] = d;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('err');
+    i--;
+  }
+}
 
+if (personalMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
+
+// вторым циклом
+// let i = 0; 
+// while (i < 2) {
+// 	i++;
+//   const a = prompt('Один из просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', '');
+
+//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log('done');
+//   } else {
+//     console.log('err');
+//     i--;
+//   }
+// }
+
+// третьим циклом
+// let i = 0; 
+// do {
+// 	i++;
+//   const a = prompt('Один из просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', '');
+
+//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log('done');
+//   } else {
+//     console.log('err');
+//     i--;
+//   }
+// }
+// while (i < 2);
+
+
