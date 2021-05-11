9d1db10689fef2d0f437f345254f90520d04325e
Ivan <fenomen_ny@mail.ru>
2021-05-11 13:11:46 +0300

    method

---
+++ js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// const str = 'teS t';
+
+// console.log(str[2] = 'd');
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
+
+// const fruit = 'somes fruit';
+
+// console.log(fruit.indexOf('s'));
+
+// const logg = 'Hello world';
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+// console.log(logg.substr(6, 5));
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+const text = "12.2px";
+// console.log(parseInt(text));
+console.log(parseFloat(text));
