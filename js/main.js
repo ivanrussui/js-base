0b5872af7a2f54182a0b5a3f27a992273ddd3d9c
Ivan <fenomen_ny@mail.ru>
2021-05-20 19:43:29 +0300

    forEach

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 'use strict';
 
+
 const arr = [1, 2, 3, 6, 8];
-arr[99] = 0;
-console.log(arr.length);
-console.log(arr);
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+arr.forEach(function(item, i, arr) {
+	console.log(`${i}: ${item} внутри массива ${arr}`);
+});
 
 // arr.pop();
 // arr.push(9);
-
 
 // for (let i = 0; i < arr.length; i++) {
 // 	console.log(arr[i]);
@@ -40,7 +44,6 @@ console.log(arr);
 // // }
 
 // // learnJS('JavaScript', done);
-
 
 // // тут я практикуюсь
 // // function myFirst(text) {
@@ -130,4 +133,3 @@ console.log(arr);
 // // }
 
 // // console.log(counter);
-
