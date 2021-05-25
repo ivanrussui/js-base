61484c303dd2438ffc5c613e52da007aaf6a9e81
Ivan <fenomen_ny@mail.ru>
2021-05-25 16:50:35 +0300

    dynamic

--- js/main.js
+++ js/main.js
@@ -1,24 +1,59 @@
 'use strict';
 
-function hello() {
-	console.log('hello world');
-}
+// To String
 
-hello();
+// 1)
+// console.log(typeof(String(null)));
+// console.log(typeof(String(4)));
 
-function hi() {
-	console.log('say hi');
-	// debugger;
-}
+// 2)
+// console.log(typeof(5 + ''));
+// console.log(typeof(null + ''));
+// console.log(typeof(undefined + ''));
+// console.log(typeof(false + ''));
+// console.log(typeof(Array + ''));
 
-hi();
+// const num = 5;
 
-const arr = [1, 14, 4, 30, 54],
-			sorted = arr.sort(compareNum);
+// console.log('https://vk.com/catalog/' + num);
 
-function compareNum(a, b) {
-	return a - b;
-}
+// const fontSize = 26 + 'px';
 
-console.log(sorted);
+// console.log(fontSize);
 
+
+// To Number
+
+// 1)
+// console.log(typeof(Number('4')));
+
+// 2)
+// console.log(typeof(+'5'));
+
+// 3)
+// console.log(typeof(parseInt('15px', 10)));
+
+// let answ = +prompt('Hello', '');
+
+
+// To boolean
+
+// 0, '', null, undefined, NaN; - false
+// 1)
+// let switcher = null;
+
+// if (switcher) {
+// 	console.log('Working...');
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+// 	console.log('Working...');
+// }
+
+// 2)
+console.log(typeof(Boolean('4')));
+
+// 3)
+console.log(typeof(!!'44'));
